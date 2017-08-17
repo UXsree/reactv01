87b2b62a3c8f8278dba6bd983800c37bd2fe027e
Sreekanth Sundareswaran <ssundare@global.sial.com>
2017-08-17 16:44:55 +0530

    created components

---
+++ src/components/homePage.js
@@ -0,0 +1,17 @@
+"use strict";
+
+var React = require('react');
+
+var Home = React.createClass({
+    render: function(){
+        return (
+            <div className="jumbotron">
+                <h1>sree's first app</h1>
+                <p> setting up all together </p>    
+            </div>
+        );
+    }
+});
+
+
+module.exports = Home;
