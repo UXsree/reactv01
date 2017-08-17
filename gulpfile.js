055aee48cc40edf350adfdcfc565685c04c1d68d
Sreekanth Sundareswaran <ssundare@global.sial.com>
2017-08-17 16:36:07 +0530

    created components

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 var concat = require('gulp-concat');
+var lint = require('gulp-eslint');
 
 var config={
     port: 3000,
@@ -54,9 +55,21 @@ gulp.task('js',function(){
         .pipe(connect.reload());
 });
 
-gulp.task('watch', function(){
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+gulp.task('css',function(){
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('default',['html', 'js', 'open', 'watch']);
+gulp.task('lint', function(){
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+});
+
+gulp.task('watch', function(){
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js','lint']);
+});
+
+gulp.task('default',['html', 'js', 'css', 'lint', 'open', 'watch']);
